c5c411b3449b1aa0a8b48b7c7741f6f5c240b72d
juandavid7777 <juandavid7777@gmail.com>
2022-07-19 14:04:30 +0400

    test

--- pages/Weather_data.py
+++ pages/Weather_data.py
@@ -43,35 +43,35 @@ df_map = pd.DataFrame({"lat":[stat_lat, option_0_lat], "lon":[stat_lon, option_0
 st.map(df_map)
 
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+# df = pd.DataFrame(
+#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+#     columns=['lat', 'lon'])
 
-st.pydeck_chart(pdk.Deck(
-     map_style='mapbox://styles/mapbox/light-v9',
-     initial_view_state=pdk.ViewState(
-         latitude=37.76,
-         longitude=-122.4,
-         zoom=11,
-         pitch=50,
-     ),
-     layers=[
-         pdk.Layer(
-            'HexagonLayer',
-            data=df,
-            get_position='[lon, lat]',
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            pickable=True,
-            extruded=True,
-         ),
-         pdk.Layer(
-             'ScatterplotLayer',
-             data=df,
-             get_position='[lon, lat]',
-             get_color='[200, 30, 0, 160]',
-             get_radius=200,
-         ),
-     ],
- ))
+# st.pydeck_chart(pdk.Deck(
+#      map_style='mapbox://styles/mapbox/light-v9',
+#      initial_view_state=pdk.ViewState(
+#          latitude=37.76,
+#          longitude=-122.4,
+#          zoom=11,
+#          pitch=50,
+#      ),
+#      layers=[
+#          pdk.Layer(
+#             'HexagonLayer',
+#             data=df,
+#             get_position='[lon, lat]',
+#             radius=200,
+#             elevation_scale=4,
+#             elevation_range=[0, 1000],
+#             pickable=True,
+#             extruded=True,
+#          ),
+#          pdk.Layer(
+#              'ScatterplotLayer',
+#              data=df,
+#              get_position='[lon, lat]',
+#              get_color='[200, 30, 0, 160]',
+#              get_radius=200,
+#          ),
+#      ],
+#  ))
